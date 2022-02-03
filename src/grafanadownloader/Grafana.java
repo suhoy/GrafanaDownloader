1ca16e053ce5db72cffcd17f995460e4abe79ca9
suhoy <suh1995@mail.ru>
2022-02-03 22:52:34 +0300

    timeout 5 min

--- src/grafanadownloader/Grafana.java
+++ src/grafanadownloader/Grafana.java
@@ -13,17 +13,17 @@ import java.util.List;
  * @author @suhoy Sergey Sukhorukov
  */
 public class Grafana {
-    
+
     public Props conf;
     public String start;
     public String finish;
-    
+
     public Grafana(Props conf, String start, String finish) {
         this.conf = conf;
         this.start = Utils.DateIntoTimestamp(start, conf.params.get("java.timeMask"));
         this.finish = Utils.DateIntoTimestamp(finish, conf.params.get("java.timeMask"));
     }
-    
+
     public void DownloadAll() {
         System.out.println("\r\nDownloading: ");
         String host = conf.params.get("grafana.host");
@@ -33,17 +33,17 @@ public class Grafana {
         String width = conf.params.get("panels.width");
         String urls;
         HttpURLConnection conn;
-        
+
         File file = new File(System.getProperty("user.dir") + "\\" + conf.outFolder);
         file.mkdir();
-        try {
-            
-            for (int i = 1; i < conf.metrics.size() + 1; i++) {
+
+        for (int i = 1; i < conf.metrics.size() + 1; i++) {
+            try {
                 List<String> vars = new ArrayList<>();
                 int j = 1;
-                while (true) {                    
+                while (true) {
                     String var = conf.metrics.get(i - 1).get("var" + j);
-                 
+
                     if (var == null) {
                         break;
                     }
@@ -63,32 +63,35 @@ public class Grafana {
                         height,
                         width,
                         vars);
-                
+
                 System.out.println("Link" + i + "=" + urls);
                 URL url = new URL(urls);
-                                
+
+                
                 conn = (HttpURLConnection) url.openConnection();
                 conn.setRequestMethod("GET");
-                conn.setConnectTimeout(1_000*60*5);
+                conn.setReadTimeout(1_000 * 60 * 5);
+                conn.setConnectTimeout(1_000 * 60 * 5);
+                
+
                 conn.setRequestProperty("Accept", "application/json");
                 conn.setRequestProperty("Authorization", "Bearer " + conf.params.get("grafana.token"));
-              
-                
+
                 if (conn.getResponseCode() != 200) {
                     throw new RuntimeException("Failed : HTTP error code : "
                             + conn.getResponseCode());
                 }
-                
+
                 BufferedImage image = ImageIO.read(conn.getInputStream());
                 String outImage = System.getProperty("user.dir") + "\\" + conf.outFolder + "\\" + conf.metrics.get(i - 1).get("output");
                 //Also extension for image : conf.metrics.get(i - 1).get("output").split("\\.")[1]
                 ImageIO.write(image, "png", new File(outImage));
                 System.out.println("Succesfull for: " + outImage);
                 conn.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+
     }
 }
